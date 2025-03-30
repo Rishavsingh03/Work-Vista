4c91c19bec47e29926aecf2be6a0e6ee727f9d0a
Rishavsingh03 <rishavkumar3864@gmail.com>
2025-03-31 01:16:01 +0530

    Minor correction in main

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,16 +2,21 @@ import React, { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import toast, { Toaster } from 'react-hot-toast';
-// import { Provider } from 'react-redux'
-// import store from './redux/store.js'
+import { Toaster } from 'sonner'
+import { Provider } from 'react-redux'
+import store from './redux/store.js'
+import { persistStore } from 'redux-persist'
+import { PersistGate } from 'redux-persist/integration/react'
 
+const persistor =persistStore(store);
 
 createRoot(document.getElementById('root')).render(
   < React.StrictMode>
-  // <Provider store={store}>
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
     <App />
-    //  </Provider>
-    <Toaster />
-  </React.StrictMode>
+    </PersistGate>
+     </Provider>
+    <Toaster></Toaster>
+  </React.StrictMode>,
 )
