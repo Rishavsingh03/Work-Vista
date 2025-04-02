b28face7fdaa9a550516254f302c2b0ac6feda7f
Rishavsingh03 <rishavkumar3864@gmail.com>
2025-04-03 00:29:13 +0530

    Complete user funcationality

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,8 +1,8 @@
 import { User } from "../models/user.model.js";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
-// import getDataUri from "../utils/datauri.js";
-// import cloudinary from "../utils/cloudinary.js";
+import getDataUri from "../utils/datauri.js";
+import cloudinary from "../utils/cloudinary.js";
  
 
 export const register= async(req,res)=>{
@@ -16,9 +16,9 @@ export const register= async(req,res)=>{
                 success:false
             });
         };
-        // const file=req.file
-        // const fileUri=getDataUri(file);
-        // const cloudResponse= await cloudinary.uploader.upload(fileUri.content);
+        const file=req.file
+        const fileUri=getDataUri(file);
+        const cloudResponse= await cloudinary.uploader.upload(fileUri.content);
 
         const user =await User.findOne({email});
         if(user){
@@ -34,9 +34,9 @@ export const register= async(req,res)=>{
             phoneNumber,
             password:hashedPassword,
             role,
-            // profile:{
-            //     profilePhoto: cloudResponse.secure_url,
-            // }
+            profile:{
+                profilePhoto: cloudResponse.secure_url,
+            }
         })
 
         return res.status(201).json({
