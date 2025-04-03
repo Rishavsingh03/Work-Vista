045f3d571623ada99a945b11dad6d8943bc4cdaf
Rishavsingh03 <rishavkumar3864@gmail.com>
2025-04-03 19:09:14 +0530

    Final ui corrections

--- frontend/src/components/LatestJobCards.jsx
+++ frontend/src/components/LatestJobCards.jsx
@@ -1,14 +1,23 @@
 import { useNavigate } from 'react-router-dom'
 import { Badge } from './ui/badge'
 import React from 'react'
+import { Avatar, AvatarImage } from '@radix-ui/react-avatar';
+import { Button } from './ui/button';
 
 const LatestJobCards=({job})=> {
     const navigate=useNavigate();
   return (
     <div  onClick={()=>navigate(`/description/${job._id}`)} className='p-5 rounded-md shadow-xl bg-white border border-gray-100 cursor-pointer'>
             <div>
-                <h1 className='font-medium text-lg'>{job?.company?.name}</h1>
-                <p className='text-sm text-gray-500'>India</p>
+                <div className='flex align-middle '>
+                       <div className="w-10 h-10 -mx-px" variant="outline" size="icon">
+                            <Avatar>
+                                <AvatarImage src={job?.company?.logo} />
+                            </Avatar>
+                        </div>
+                        <h1 className='font-medium text-lg justify-center mx-6'>{job?.company?.name}</h1>
+                </div>
+                <p className='text-sm text-gray-500 mx-16'>India</p>
             </div>
             <div>
                 <h1 className='font-bold text-lg my-2'>{job?.title}</h1>
